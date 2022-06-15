2893e8236bd421f40d739ae94f0f6db086209aac
buhe <bugu1986@126.com>
2022-06-15 11:11:35 +0800

    fix error

--- F1.sol
+++ F1.sol
@@ -1,17 +1,18 @@
-// Our first contract is a faucet!
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.9.0;
 contract Faucet {
 
     // Give out ether to anyone who asks
-    function withdraw(uint withdraw_amount) public{
+    function withdraw(uint withdraw_amount) public payable {
 
         // Limit withdrawal amount
         require(withdraw_amount <= 100000000000000000);
 
         // Send the amount to the address that requested it
-        msg.sender.transfer(withdraw_amount);
+        payable(msg.sender).transfer(withdraw_amount);
     }
 
     // Accept any incoming amount
-    function receive() public payable {}
+    receive() external payable {}
 
 }
